1b63f6e5a75191603d024c5fa15b57f9757ffd31
matt mcgregor <mattjackmcgregor@gmail.com>
2024-08-16 12:58:39 +1200

    fixed ts error as was failing when runing prod build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,9 +68,9 @@ const config = {
           to: { height: "0" },
         },
         scroll: {
-          "0%": { opacity: 0 },
-          "10%": { transform: "translateY(0)", opacity: 1 },
-          "100%": { transform: "translateY(15px)", opacity: 0 },
+          "0%": { opacity: "0" },
+          "10%": { transform: "translateY(0)", opacity: "1" },
+          "100%": { transform: "translateY(15px)", opacity: "0" },
         },
       },
       animation: {
