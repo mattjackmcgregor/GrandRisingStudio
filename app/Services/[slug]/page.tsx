f9b9e44e53db62bd5f6cc1aa31d7b9ac551f63ff
matt mcgregor <mattjackmcgregor@gmail.com>
2024-09-10 21:46:03 +1200

    service specialist links to the individual team page.

--- app/Services/[slug]/page.tsx
+++ app/Services/[slug]/page.tsx
@@ -4,9 +4,9 @@ import React from "react";
 import { notFound } from "next/navigation";
 import { serviceData } from "@/data/serviceData";
 import CloudinaryImage from "../../components/CloudinaryImage";
-import CloudinaryVideoService from "../../components/CloudinaryVideo";
 
 import dynamic from "next/dynamic";
+import Link from "next/link";
 
 const DynamicCloudinaryVideoService = dynamic(
   () => import("../../components/CloudinaryVideo"),
@@ -65,26 +65,33 @@ const ServicePage = ({ params }: { params: { slug: string } }) => {
             Our {service.service} Specialists
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {/* Replace with actual data */}
-            {[1, 2, 3].map((specialist) => (
-              <div
-                key={specialist}
-                className="bg-transparent border border-gray-600 transform hover:scale-105 transition-all duration-300 p-4 rounded-lg"
+            {service.specialists.map((specialist) => (
+              <Link
+                href={`/team/${specialist.urlPath}`}
+                key={specialist.urlPath}
+                className="bg-transparent border border-gray-600 transform hover:scale-105 cursor-pointer transition-all duration-300 p-4 rounded-lg"
               >
                 <CloudinaryImage
-                  src="//compressedLogo_lq4ksg" // Replace with actual image
-                  alt={`Specialist ${specialist}`}
+                  src="//compressedLogo_lq4ksg" // @TODO: Replace with actual image of person
+                  alt={`Specialist ${specialist.name}`}
                   width={200}
                   height={200}
                   extraClasses="w-full h-48 object-cover rounded-lg mb-4"
                 />
                 <h3 className="text-xl font-semibold mb-2">
-                  Specialist {specialist}
+                  {specialist.name}
                 </h3>
-                <p className="text-gray-300">
-                  Specializes in various styles and techniques
-                </p>
-              </div>
+                <ul className="flex">
+                  {specialist.skills.map((skill, index) => (
+                    <li key={skill} className="text-gray-300 italic text-sm">
+                      {skill}
+                      {index < specialist.skills.length - 1 && (
+                        <span className="mx-1">|</span>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </Link>
             ))}
           </div>
         </div>
--- data/serviceData.ts
+++ data/serviceData.ts
@@ -4,17 +4,68 @@ export const serviceData = [
     description:
       "Sickest cuts in the game. sometimes hori, somethimes good. nach just Kidding but they alowaays booked up. thye on.",
     videoUrl: "sampleBarberCompressed2_sbysnm",
+    specialists: [
+      {
+        name: "John Doe",
+        urlPath: "john-doe",
+        skills: ["fades", "tattoos", "haircuts"],
+      },
+      {
+        name: "Jane Smith",
+        urlPath: "jane-smith",
+        skills: ["fades", "tattoos", "haircuts"],
+      },
+      {
+        name: "Michael Johnson",
+        urlPath: "michael-johnson",
+        skills: ["fades", "tattoos", "haircuts"],
+      },
+    ],
   },
   {
     service: "Tattoo",
     description:
       "bros pretty good. if you do a no show you're ugly. and pussio.. something about quality and line work. maybe the different styles and that",
     videoUrl: "sampleTattooCompressed2_rlwowh",
+    specialists: [
+      {
+        name: "John Doe",
+        urlPath: "john-doe",
+        skills: ["fades", "tattoos", "haircuts"],
+      },
+      {
+        name: "Jane Smith",
+        urlPath: "jane-smith",
+        skills: ["fades", "tattoos", "haircuts"],
+      },
+      {
+        name: "Michael Johnson",
+        urlPath: "michael-johnson",
+        skills: ["fades", "tattoos", "haircuts"],
+      },
+    ],
   },
   {
     service: "Design",
     description:
       "design and art. murals and graff and other shit i think. give heaps of money to make stuff look on.",
     videoUrl: "sampleDesignCompressed2_qbcadr",
+    specialists: [
+      {
+        name: "John Doe",
+        urlPath: "john-doe",
+        skills: ["fades", "tattoos", "haircuts"],
+      },
+      {
+        name: "Jane Smith",
+        urlPath: "jane-smith",
+        skills: ["fades", "tattoos", "haircuts"],
+      },
+      {
+        name: "Michael Johnson",
+        urlPath: "michael-johnson",
+        skills: ["fades", "tattoos", "haircuts"],
+      },
+    ],
   },
 ];
